28e42446ec8748995a389404fa940ae9199df78c
Daniel García Rodríguez <d.grodriguez@alumnos.upm.es>
2026-09-19 23:20:56 +0200

    feat: capable of firing bullets
    Hecho el video 5 donde se explica como poder disparar balas siguiendo el movimiento de la nave.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     public float thrutForce = 10f;
     public float rotationSpeed = 120f;
 
+    public GameObject gun, bulletPrefab;
+
     private Rigidbody _rigid;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,5 +27,14 @@ public class Player : MonoBehaviour
         _rigid.AddForce(thrustDirection * thrust * thrutForce);
 
         transform.Rotate(Vector3.forward, -rotation * rotationSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, gun.transform.position, Quaternion.identity);
+
+            Bullet bulletScript = bullet.GetComponent<Bullet>();
+
+            bulletScript.targetVector = transform.right;
+        }
     }
 }
